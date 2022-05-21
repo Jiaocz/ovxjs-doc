3d7afb98dad829547df1f46da2fb55d1b1665153
橙梓 <me@orangii.cn>
2022-05-21 22:45:14 +0800

    Update PWA

--- docs/.vuepress/client.js
+++ docs/.vuepress/client.js
@@ -1,7 +1,11 @@
 import { defineClientConfig } from "@vuepress/client"
+import { usePwaEvent } from "@vuepress/plugin-pwa/lib/client"
 
 export default defineClientConfig({
     enhance({ app, router, siteData }) {},
-    setup() {},
+    setup() {
+        const event = usePwaEvent()
+        event.on('updated', registration => registration.update())
+    },
     rootComponents: [],
 })
